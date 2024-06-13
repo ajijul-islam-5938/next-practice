d7a598954d3071fd102f5f9f8d45c084d4a471f3
shafiul-osd <mdshafiulislamsafi2506@gmail.com>
2024-06-13 20:39:20 +0600

    updated(2)

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,17 @@
 import Image from "next/image";
 
 
-const getTime = async ()=>{
-                                                    // {next : {revalidate : 5}}
-                                                    // chache : "no-store"
-  const res = await fetch("http://localhost:3000/time"); 
-  const data = await res.json();
-  return data.currentTime
-}
+// const getTime = async ()=>{
+//                                               // {next : {revalidate : 5}}
+//   const res = await fetch("http://localhost:3000/time",{cache : "no-store"}); 
+//   const data = await res.json();
+//   return data.currentTime
+// }
 export default async function Home() {
   const currentTime =await getTime()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        {currentTime}
+        Home Page
       
     </main>
   );
