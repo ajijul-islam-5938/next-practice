67bc80dcbee513333533e1ae3e1301edeca20a7c
shafiul-osd <mdshafiulislamsafi2506@gmail.com>
2024-06-13 20:34:25 +0600

    upadted(1)

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 
 const getTime = async ()=>{
                                                     // {next : {revalidate : 5}}
-  const res = await fetch("http://localhost:3000/time",{cache : "no-store"}); 
+                                                    // chache : "no-store"
+  const res = await fetch("http://localhost:3000/time"); 
   const data = await res.json();
   return data.currentTime
 }
